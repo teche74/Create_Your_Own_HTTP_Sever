f309145dc1bc77bb00afae7e744b7fbcd5dd35b1
Ujjwal <ujjwalbisht55@gmail.com>
2024-03-17 13:10:35 +0530

    pass 2nd stage

--- src/server.cpp
+++ src/server.cpp
@@ -51,8 +51,11 @@ int main(int argc, char **argv)
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
+  std::string resp = std::string("HTPP/1.1 200 OK\r\n\r\n").c_str();
+
+  send(client_fd,resp.c_str(),resp.length(),0);
   
   close(server_fd);
 
